8e2af1944504c729ba0336f3710dc32923b705e6
Weverton Martins <wevertonm22@icloud.com>
2026-03-30 10:30:41 -0300

    verificar se os dados foram importados corretamente

---
+++ Tabelas/Dados/Verificando Importação dos Dados/Verficando se os Dados Forando Inseridos.sql
@@ -0,0 +1,7 @@
+SELECT * from alunosalunos;
+SELECT * from disciplinasdisciplinas;
+SELECT * from notasnotas;
+SELECT * from professoresprofessores;
+SELECT * from turma_alunosturma_alunos;
+SELECT * from turma_disciplinasturma_disciplinas;
+SELECT * from turmasturmas;
