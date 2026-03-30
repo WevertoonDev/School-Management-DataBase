537ea06e131ee8abccb3ccba0d2bd20665ae3371
Weverton Martins <wevertonm22@icloud.com>
2026-03-30 10:06:31 -0300

    Realizar consultas simples

---
+++ Realizar consultas simples.sql
@@ -0,0 +1,4 @@
+SELECT * from alunosalunos order by nome_aluno ASC;
+SELECT * FROM disciplinasdisciplinas WHERE carga_horaria >= 40;
+SELECT * from notasnotas WHERE nota > 6 and nota < 8;
+SELECT * from notasnotas WHERE nota BETWEEN 7 and 8;
